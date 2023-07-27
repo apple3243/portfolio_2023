c35f193afbd17198a7164f0401ef9a7eaca4e5a7
apple3243 <dlwldus12341234@gmail.com>
2023-07-27 15:06:18 -0700

    updated portfolio / resume file

--- src/asset/About.js
+++ src/asset/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import resume from '../asset/resume.pdf';
 
 function About(){
     return(
         <div className="about">
             <h3 className="about-title">About Me</h3>
             <p className="about-desc">Hi there! My name is Jiyeon Lee, and I am a passionate front-end developer with over 2 years of professional experience. I have a love for learning and thrive in environments where I can constantly challenge myself to grow and develop new skills. I take pride in my pixel-perfect attention to detail and ability to deliver high-quality work quickly. As a quick learner, I am always eager to take on new challenges and push the boundaries of what I can achieve. Thanks for stopping by my portfolio, and I look forward to connecting with you!</p>
-            <a href="./resume.pdf" className="resume-link" download>Resume</a>
+            <a href={resume} className="resume-link" download>Resume</a>
         </div>
     )
 }
--- src/asset/Portfolio.js
+++ src/asset/Portfolio.js
@@ -5,10 +5,11 @@ function Portfolio(){
         <div className="portfolio">
             <h3 className="title">Portfolio</h3>
             <ul className="list">
-                <li className="whale-on"><a href="https://campaign.naver.com/whaleonstudy/" target="_blank"><span className="vh">WhaleOn Campaign Website</span></a></li>
-                <li className="netflix"><a href="https://m.search.naver.com/p/n.search/csearch/content/eprender.nhn?q=%EC%98%A4%EC%A7%95%EC%96%B4%20%EA%B2%8C%EC%9E%84&where=m&pkid=356" target="_blank"><span className="vh">Naver Search Netflix Series End Page</span></a></li>
-                <li className="hanguel"><a href="https://hangeul.naver.com/font/maru" target="_blank"><span className="vh">Hanguel font Website</span></a></li>
-                <li className="gratitude"><a href="https://apple3243.github.io/dailyGratitude/" target="_blank"><span className="vh">Daily Gratitude Website</span></a></li>
+                <li className="whale-on"><a href="https://campaign.naver.com/whaleonstudy/" target="_blank" rel="noreferrer"><span className="vh">WhaleOn Campaign Website</span></a></li>
+                <li className="netflix"><a href="https://m.search.naver.com/p/n.search/csearch/content/eprender.nhn?q=%EC%98%A4%EC%A7%95%EC%96%B4%20%EA%B2%8C%EC%9E%84&where=m&pkid=356" target="_blank" rel="noreferrer"><span className="vh">Naver Search Netflix Series End Page</span></a></li>
+                <li className="hanguel"><a href="https://hangeul.naver.com/font/maru" target="_blank" rel="noreferrer"><span className="vh">Hanguel font Website</span></a></li>
+                <li className="gratitude"><a href="https://apple3243.github.io/dailyGratitude/" target="_blank" rel="noreferrer"><span className="vh">Daily Gratitude Website</span></a></li>
+                <li className="ourhome"><a href="https://apple3243.github.io/ourhome/" target="_blank" rel="noreferrer"><span className="vh">OurHome Website</span></a></li>
             </ul>
         </div>
     )
