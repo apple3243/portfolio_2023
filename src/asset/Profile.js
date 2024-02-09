d6c2ccc01c6bc94c36ac2fbcecc64e225b38d326
apple3243 <dlwldus12341234@gmail.com>
2024-02-09 12:11:23 -0800

    update resume and email address

--- src/asset/Profile.js
+++ src/asset/Profile.js
@@ -9,7 +9,7 @@ function Profile(){
                 <h1 className="my-name">Jiyeon Lee (she/her)</h1>
                 <h2 className="title">Front End Developer</h2>
             </div>
-            <a href="mailto:dlwldus12341234@gmail.com" className="email-link">Email me</a>
+            <a href="mailto:jiyeon.lee621@gmail.com" className="email-link">Email me</a>
             <div className="link-box">
                 <a href="https://www.linkedin.com/in/jiyeon3243/" className="linked-in" target="_blank"><span className="vh">linkedin</span></a>
                 <a href="https://github.com/apple3243" className="github" target="_blank"><span className="vh">github</span></a>
